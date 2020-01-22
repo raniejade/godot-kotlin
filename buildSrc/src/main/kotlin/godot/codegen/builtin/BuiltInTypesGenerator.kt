718048f45d9c6f177f267744766e2696abfa7bda
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-01-22 21:18:44 +1100

    Reindent

--- buildSrc/src/main/kotlin/godot/codegen/builtin/BuiltInTypesGenerator.kt
+++ buildSrc/src/main/kotlin/godot/codegen/builtin/BuiltInTypesGenerator.kt
@@ -7,50 +7,50 @@ import godot.codegen.builtin.model.BuiltInType
 import java.io.File
 
 class BuiltInTypesGenerator {
-    private val mapper = jacksonObjectMapper()
+  private val mapper = jacksonObjectMapper()
 
-    fun generate(source: File, outputDir: File) {
-        val types = parseJson(source)
-        types.map(this::generateType)
-            .forEach { fs ->
-                fs.writeTo(outputDir)
-            }
-    }
+  fun generate(source: File, outputDir: File) {
+    val types = parseJson(source)
+    types.map(this::generateType)
+      .forEach { fs ->
+        fs.writeTo(outputDir)
+      }
+  }
 
-    private fun generateType(type: BuiltInType): FileSpec {
-        val builtInClassName = ClassName(GDNATIVE_PACKAGE, type.builtinType)
-        return FileSpec.builder(BASE_PACKAGE, type.name)
-            .addType(
-                TypeSpec.classBuilder(type.name)
-                    .primaryConstructor(
-                        FunSpec.constructorBuilder()
-                            .addParameter("context", GDCONTEXT_CLASS_NAME, KModifier.PRIVATE)
-                            .addParameter("handle", builtInClassName, KModifier.INTERNAL)
-                            .build()
-                    ).addProperty(
-                        PropertySpec.builder("context", GDCONTEXT_CLASS_NAME)
-                            .initializer("context")
-                            .build()
-                    ).addProperty(
-                        PropertySpec.builder("handle", builtInClassName)
-                            .initializer("handle")
-                            .build()
-                    ).addType(
-                        TypeSpec.companionObjectBuilder()
-                            .build()
-                    )
-                    .build()
-            ).addComment("GENERATED FILE DO NOT EDIT")
-            .build()
-    }
+  private fun generateType(type: BuiltInType): FileSpec {
+    val builtInClassName = ClassName(GDNATIVE_PACKAGE, type.builtinType)
+    return FileSpec.builder(BASE_PACKAGE, type.name)
+      .addType(
+        TypeSpec.classBuilder(type.name)
+          .primaryConstructor(
+            FunSpec.constructorBuilder()
+              .addParameter("context", GDCONTEXT_CLASS_NAME, KModifier.PRIVATE)
+              .addParameter("handle", builtInClassName, KModifier.INTERNAL)
+              .build()
+          ).addProperty(
+            PropertySpec.builder("context", GDCONTEXT_CLASS_NAME)
+              .initializer("context")
+              .build()
+          ).addProperty(
+            PropertySpec.builder("handle", builtInClassName)
+              .initializer("handle")
+              .build()
+          ).addType(
+            TypeSpec.companionObjectBuilder()
+              .build()
+          )
+          .build()
+      ).addComment("GENERATED FILE DO NOT EDIT")
+      .build()
+  }
 
-    private fun parseJson(source: File): List<BuiltInType> {
-        return mapper.readValue(source)
-    }
+  private fun parseJson(source: File): List<BuiltInType> {
+    return mapper.readValue(source)
+  }
 
-    companion object {
-        const val BASE_PACKAGE = "godot"
-        const val GDNATIVE_PACKAGE = "gdnative"
-        val GDCONTEXT_CLASS_NAME = ClassName(BASE_PACKAGE, "GDContext")
-    }
+  companion object {
+    const val BASE_PACKAGE = "godot"
+    const val GDNATIVE_PACKAGE = "gdnative"
+    val GDCONTEXT_CLASS_NAME = ClassName(BASE_PACKAGE, "GDContext")
+  }
 }
