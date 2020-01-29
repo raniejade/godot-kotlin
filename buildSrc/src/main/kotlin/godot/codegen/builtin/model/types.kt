53c072dd53d3443e77fdae20320cab19a0a2f447
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-01-29 17:19:22 +1100

    Remove old codegen code

--- buildSrc/src/main/kotlin/godot/codegen/builtin/model/types.kt
+++
@@ -1,8 +0,0 @@
-package godot.codegen.builtin.model
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class Type(@JsonProperty val pkg: String, @JsonProperty val name: String)
-data class Parameter(@JsonProperty val name: String, @JsonProperty val type: Type, @JsonProperty val isPointer: Boolean)
-data class Constructor(@JsonProperty val params: List<Parameter>, @JsonProperty val internalConstructor: String)
-data class BuiltInType(@JsonProperty val name: String, @JsonProperty val builtinType: String, @JsonProperty val constructors: List<Constructor> = emptyList())
