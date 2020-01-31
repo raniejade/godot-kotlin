7cd494a536cf1a6be27a5f1d80cae9e87d10559b
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-01-31 21:03:03 +1100

    Add signal classes

--- src/nativeMain/kotlin/godot/core/signalRegistry.kt
+++ src/nativeMain/kotlin/godot/core/signalRegistry.kt
@@ -2,7 +2,32 @@ package godot.core
 
 import godot.Object
 
-abstract class Signal(val emitter: Object) {
-  fun emit(args: VariantArray) {
+abstract class Signal(val name: String, val emitter: Object) {
+  protected fun emitSignal(vararg args: Any?) {
+    emitter.emitSignal(name, *args)
+  }
+}
+
+class Signal0(name: String, emitter: Object): Signal(name, emitter) {
+  operator fun invoke() {
+    emitSignal(name)
+  }
+}
+
+class Signal1<A>(name: String, emitter: Object): Signal(name, emitter) {
+  operator fun invoke(a: A) {
+    emitSignal(name, a)
+  }
+}
+
+class Signal2<A, B>(name: String, emitter: Object): Signal(name, emitter) {
+  operator fun invoke(a: A, b: B) {
+    emitSignal(name, a, b)
+  }
+}
+
+class Signal3<A, B, C>(name: String, emitter: Object): Signal(name, emitter) {
+  operator fun invoke(a: A, b: B, c: C) {
+    emitSignal(name, a, b, c)
   }
 }
