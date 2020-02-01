c3b76869269586914dd33b8eede45c915f2e8f6e
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-02-01 17:06:38 +1100

    Add comments to RotatingCube sample

--- samples/rotating-cube/src/rotatingCubeMain/kotlin/RotatingCube.kt
+++ samples/rotating-cube/src/rotatingCubeMain/kotlin/RotatingCube.kt
@@ -4,7 +4,11 @@ import kotlinx.cinterop.COpaquePointer
 
 class RotatingCube(handle: COpaquePointer): Spatial(handle) {
   var rotationSpeed by floatProperty()
+  // signal emitted when reverse property is changed
   val signalReverseChanged by signal1<Boolean>()
+  // properties optionally accepts a lambda a.k.a ChangeListener, that is called when
+  // the value of the property changes.
+  // signals can be converted to a ChangeListener, via <signal>.asListener()
   var reverse by booleanProperty(signalReverseChanged.asListener())
 
   override fun _onInit() {
@@ -30,10 +34,16 @@ class RotatingCube(handle: COpaquePointer): Spatial(handle) {
     // rotationDegrees = rotation
   }
 
+  // this is attached to Timer::timeout signal via the editor
+  fun flipRotation() {
+    reverse = !reverse
+  }
+
   companion object: GodotClass<Spatial, RotatingCube>(::RotatingCube) {
     override fun init(registry: ClassMemberRegistry<RotatingCube>) {
       with(registry) {
         registerMethod(RotatingCube::_process)
+        registerMethod(RotatingCube::flipRotation)
         registerProperty(RotatingCube::rotationSpeed, 100f, hint =  PropertyHint.range(0f, 1000f, 0.5f))
         registerProperty(RotatingCube::reverse)
         registerSignal(RotatingCube::signalReverseChanged, "reverse")
