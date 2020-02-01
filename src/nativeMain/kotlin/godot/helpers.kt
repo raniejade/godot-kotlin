3a8c598040f9b47b2b8cf8f5432543f61d6f83e8
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-02-01 21:01:10 +1100

    Add cast helper

--- src/nativeMain/kotlin/godot/helpers.kt
+++ src/nativeMain/kotlin/godot/helpers.kt
@@ -1,9 +1,15 @@
 package godot
 
+import kotlinx.cinterop.COpaquePointer
+
 typealias ChangeListener<T, R> = T.(R?, R) -> Unit
 
 fun <T: Object, A0> Signal1<A0>.asListener(): ChangeListener<T, A0> {
   return { _, newValue ->
     this@asListener.emit(this,  newValue)
   }
+}
+
+fun <T: Object> Object.castTo(factory: (COpaquePointer) -> T): T {
+  return factory(_handle)
 }
