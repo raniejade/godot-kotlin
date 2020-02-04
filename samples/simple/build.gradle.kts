eff2a4a890b1135bbf7ad6a5dd5f627e9e5ec9e0
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-02-05 00:42:09 +1100

    Port simple sample to use gradle plugin

--- samples/simple/build.gradle.kts
+++ samples/simple/build.gradle.kts
@@ -12,43 +12,23 @@ repositories {
     mavenCentral()
 }
 
-kotlin {
-    fun KotlinNativeTargetWithTests.configure() {
-        binaries {
-            sharedLib(listOf(NativeBuildType.DEBUG))
-        }
-    }
-
-    val target = if (Os.isFamily(Os.FAMILY_MAC)) {
-        macosX64("simple")
-    } else if (Os.isFamily(Os.FAMILY_WINDOWS)) {
-        mingwX64("simple")
-    } else if (Os.isFamily(Os.FAMILY_UNIX)) {
-        linuxX64("simple")
-    } else {
-        throw AssertionError("Unsupported os.")
-    }
-
-    target.configure()
-
-    sourceSets {
-        val simpleMain by getting {
-            dependencies {
-                api(project(":core"))
-            }
-        }
-    }
-}
-
 godot {
     libraries {
-        val rotatingCube by creating {
-            targets.set(
-              listOf("simple")
-            )
+        val simple by creating {
             classes.set(
               listOf("Simple")
             )
         }
     }
+}
+
+kotlin {
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+        compilations.getByName("main").defaultSourceSet {
+            dependencies {
+                api(project(":core"))
+            }
+        }
+    }
+
 }
