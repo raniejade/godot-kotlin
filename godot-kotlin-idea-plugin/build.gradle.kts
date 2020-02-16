5e0cbb3c416dc26b411838c9ebdbaf914b61d705
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-02-16 13:22:57 +1100

    Initial structure for the IDEA plugin (#36)

---
+++ godot-kotlin-idea-plugin/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+  kotlin("jvm")
+  id("org.jetbrains.intellij") version "0.4.16"
+}
+
+data class VersionRange(
+  val since: String,
+  val until: String
+)
+
+data class Product(
+  val type: String,
+  val version: String,
+  val plugins: List<String>,
+  val versionRange: VersionRange,
+  val suffix: String
+)
+
+val product = properties.getOrDefault("idea.product", "IJ193").run {
+  when (this) {
+    "IJ193" -> Product(
+      "IC",
+      "193.6494.35",
+      listOf("org.jetbrains.kotlin:1.3.61-release-IJ2019.3-1"),
+      VersionRange("193.1", "193.*"),
+      "IJ2019.3"
+    )
+    else -> throw AssertionError("Unsupported product $this")
+  }
+}
+
+repositories {
+  jcenter()
+  mavenCentral()
+}
+
+dependencies {
+  compileOnly(kotlin("stdlib"))
+}
+
+intellij {
+  pluginName = "Godot Kotlin (godot-kotl.in)"
+  version = product.version
+  type = product.type
+  setPlugins(*product.plugins.toTypedArray())
+}
+
+tasks {
+  compileKotlin {
+    kotlinOptions {
+      jvmTarget = "1.8"
+    }
+  }
+
+  val productVersion = "${project.version}-${product.suffix}"
+  buildPlugin {
+    archiveBaseName.set("")
+    archiveVersion.set(productVersion)
+  }
+
+  patchPluginXml {
+    setVersion(productVersion)
+    setSinceBuild(product.versionRange.since)
+    setUntilBuild(product.versionRange.until)
+  }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
 include(":godot-kotlin")
 include(":godot-kotlin-gradle-plugin")
+
+val excludeIdePlugins: String? by settings
+
+// gradle-intellij-plugin downloads the IJ SDK (the whole ide) during configuration phase
+// so even if it does not participate in the build it will try to download the sdk.
+// this flag exclude the idea plugin module when `excludeIdePlugins` property is present.
+if (excludeIdePlugins == null) {
+  include(":godot-kotlin-idea-plugin")
+}
