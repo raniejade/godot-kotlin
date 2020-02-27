7863b358cc5d6647d6eb9d8b06e2fb3993d5700b
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-02-27 21:25:29 +1100

    Add support for CLion (#57)

--- godot-kotlin-idea-plugin/build.gradle.kts
+++ godot-kotlin-idea-plugin/build.gradle.kts
@@ -17,7 +17,8 @@ data class Product(
   val version: String,
   val plugins: List<String>,
   val versionRange: VersionRange,
-  val suffix: String
+  val suffix: String,
+  val extraResources: String
 )
 
 val product = properties.getOrDefault("idea.product", "IJ193").run {
@@ -27,7 +28,16 @@ val product = properties.getOrDefault("idea.product", "IJ193").run {
       "193.6494.35",
       listOf("java", "org.jetbrains.kotlin:1.3.61-release-IJ2019.3-1"),
       VersionRange("193.1", "193.*"),
-      "IJ2019.3"
+      "IJ2019.3",
+      "src/intellij/resources"
+    )
+    "CL193" -> Product(
+      "CL",
+      "2019.3.4",
+      listOf("com.intellij.kotlinNative.platformDeps:193.6494.38", "org.jetbrains.kotlin.native.clion:1.3.61-release-CLion-193.6494.38-8"),
+      VersionRange("193.1", "193.*"),
+      "CL2019.3",
+      "src/clion/resources"
     )
     else -> throw AssertionError("Unsupported product $this")
   }
@@ -47,6 +57,10 @@ intellij {
   version = product.version
   type = product.type
   setPlugins(*product.plugins.toTypedArray())
+}
+
+sourceSets["main"].resources {
+  srcDir(product.extraResources)
 }
 
 tasks {
